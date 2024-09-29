35e8b5eb18c4db5023611d4d3d7ba426f29c73ee
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-09-29 14:42:29 -0400

    Move bf expr parsing to BF.Syntax

--- src/Language/BF.hs
+++ src/Language/BF.hs
@@ -1,56 +1,5 @@
 module Language.BF (
-    bf,
-    Parser,
-    runParser,
-    Expr (..),
+    module Language.BF.Syntax,
 ) where
 
-import Parser.Core (Parser, char, getPos, runParser, sat)
-
-import Control.Applicative (Alternative (..), asum)
-import Control.Monad (void)
-import Data.Functor (($>), (<&>))
-import Numeric.Natural (Natural)
-
-data Expr
-    = ShiftRight !Natural
-    | ShiftLeft !Natural
-    | Inc !Natural
-    | Dec !Natural
-    | Out
-    | In
-    | Loop !Natural [Expr]
-    | Noop
-    deriving (Show, Eq)
-
-bf :: Parser String Maybe [Expr]
-bf = filter (Noop /=) <$> many (loop <|> tok)
-
-loop :: Parser String Maybe Expr
-loop = do
-    void $ char '['
-    pos <- getPos
-    fs <- bf
-    void $ char ']'
-    pure $
-        if null fs
-            then Noop
-            else Loop pos fs
-
-func :: (Enum b, Eq a) => a -> Parser [a] Maybe b
-func c = toEnum . length <$> some (char c)
-
-tok :: Parser String Maybe Expr
-tok =
-    asum
-        [ func '>' <&> ShiftRight
-        , func '<' <&> ShiftLeft
-        , func '+' <&> Inc
-        , func '-' <&> Dec
-        , char '.' $> Out
-        , char ',' $> In
-        , comment *> pure Noop
-        ]
-
-comment :: Parser [Char] Maybe String
-comment = some $ sat (\x -> all (x /=) "><+-.,[]")
+import Language.BF.Syntax
--- src/Language/BF/Syntax.hs
+++ src/Language/BF/Syntax.hs
@@ -1,3 +1,56 @@
 module Language.BF.Syntax (
-
+    bf,
+    Parser,
+    runParser,
+    Expr (..),
 ) where
+
+import Parser.Core (Parser, char, getPos, runParser, sat)
+
+import Control.Applicative (Alternative (..), asum)
+import Control.Monad (void)
+import Data.Functor (($>), (<&>))
+import Numeric.Natural (Natural)
+
+data Expr
+    = ShiftRight !Natural
+    | ShiftLeft !Natural
+    | Inc !Natural
+    | Dec !Natural
+    | Out
+    | In
+    | Loop !Natural [Expr]
+    | Noop
+    deriving (Show, Eq)
+
+bf :: Parser String Maybe [Expr]
+bf = filter (Noop /=) <$> many (loop <|> tok)
+
+loop :: Parser String Maybe Expr
+loop = do
+    void $ char '['
+    pos <- getPos
+    fs <- bf
+    void $ char ']'
+    pure $
+        if null fs
+            then Noop
+            else Loop pos fs
+
+func :: (Enum b, Eq a) => a -> Parser [a] Maybe b
+func c = toEnum . length <$> some (char c)
+
+tok :: Parser String Maybe Expr
+tok =
+    asum
+        [ func '>' <&> ShiftRight
+        , func '<' <&> ShiftLeft
+        , func '+' <&> Inc
+        , func '-' <&> Dec
+        , char '.' $> Out
+        , char ',' $> In
+        , comment $> Noop
+        ]
+
+comment :: Parser [Char] Maybe String
+comment = some $ sat (`notElem` "><+-.,[]")
